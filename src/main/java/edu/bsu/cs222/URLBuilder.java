cd484a84c55cd5cb01eb6724b777b8a66377b5de
wtwithers2020 <wtwithers2020@gmail.com>
2021-09-23 18:45:32 -0400

    Created the URLBuilder class and generalized the code to accept a userInput

--- src/main/java/edu/bsu/cs222/URLBuilder.java
+++ src/main/java/edu/bsu/cs222/URLBuilder.java
@@ -1,18 +1,16 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+package edu.bsu.cs222;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class URLBuilderTest {
+public class URLBuilder {
 
-    @Test
-    public void stringBuilderTest(){
-        String userInput = "frank zappa";
+    public String BuildUrl(String userInput){
         String firstHalfOfUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=";
         String userInputSectionOfUrl = URLEncoder.encode(userInput, StandardCharsets.UTF_8);
         String secondHalfOfUrl = "&redirects=1&callback=&rvprop=timestamp%7Cuser&rvlimit=30";
         String completeUrl = firstHalfOfUrl+userInputSectionOfUrl+secondHalfOfUrl;
-        Assertions.assertEquals("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=frank+zappa&redirects=1&callback=&rvprop=timestamp%7Cuser&rvlimit=30",completeUrl);
+
+        return completeUrl;
     }
 }
--- src/main/java/edu/bsu/cs222/wikipediaConnector.java
+++ src/main/java/edu/bsu/cs222/wikipediaConnector.java
@@ -5,10 +5,9 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class wikipediaConnector {
-    private static URLConnection connectToWikipedia() throws IOException {
-        URL url = new URL(
-                "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp%7Cuser&rvlimit=30&titles=frank%20zappa&redirects=");
-        // %20 = space so frank%20zappa = "frank zappa"
+
+    private static URLConnection connectToWikipedia(String Url) throws IOException {
+        URL url = new URL(Url);
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent",
                 "CS222FirstProject/0.1 (wtwithers@bsu.edu)");
