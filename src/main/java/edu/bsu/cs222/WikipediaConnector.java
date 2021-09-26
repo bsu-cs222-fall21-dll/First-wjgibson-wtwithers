5419ef0d3761a553a7469755a2e52645b4ac8bec
wjgibson <wjgibson@bsu.edu>
2021-09-26 13:22:00 -0400

    Added catch statements for network error and no name error

--- src/main/java/edu/bsu/cs222/WikipediaConnector.java
+++ src/main/java/edu/bsu/cs222/WikipediaConnector.java
@@ -2,17 +2,25 @@ package edu.bsu.cs222;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ConnectException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class WikipediaConnector {
 
     private static URLConnection connectToWikipedia(String Url) throws IOException {
+        try{
             URL url = new URL(Url);
             URLConnection connection = url.openConnection();
             connection.setRequestProperty("User-Agent",
                     "CS222FirstProject/0.1 (wtwithers@bsu.edu)");
             InputStream inputStream = connection.getInputStream();
             return connection;
+        }
+        catch(ConnectException connectException){
+            System.out.println("Network Error");
+            System.exit(2);
+            return null;
+        }
     }
 }
