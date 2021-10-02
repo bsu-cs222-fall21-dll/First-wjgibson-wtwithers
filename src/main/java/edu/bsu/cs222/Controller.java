60a21e1ea55ff5a38e1acfe25cbfc3f6f18b2851
wjgibson <wjgibson@bsu.edu>
2021-10-02 18:41:23 -0400

    Began writing FXML file and setting up visual layout of GUI

--- src/main/java/edu/bsu/cs222/Controller.java
+++
@@ -1,7 +0,0 @@
-package edu.bsu.cs222;
-
-public class Controller {
-    public void getRevisions(){
-        System.out.println("Hey, This is a test buddy!");
-    }
-}
--- src/main/java/edu/bsu/cs222/GUIMain.java
+++ src/main/java/edu/bsu/cs222/GUIMain.java
@@ -1,29 +1,69 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import net.minidev.json.JSONArray;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class GUIMain extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/GUIBuilder.fxml"));
-            primaryStage.setTitle("WikipediaRevisionFetcher");
-            primaryStage.setScene(new Scene(root, 500, 475));
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    //Thank you to Zak Young and Connor Pflederer
+    // for the concept of using the FXML file as the resource
+    @Override
+    public void start(Stage primaryStage) {
+
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("GuiFormat.fxml")));
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /*private void getAllRevisionsWithInputField() throws IOException {
+        URLBuilder urlBuilder = new URLBuilder();
+        WikipediaConnector wikipediaConnector = new WikipediaConnector();
+        WikipediaMissingParser missingParser = new WikipediaMissingParser();
+        WikipediaRedirectParser redirectParser = new WikipediaRedirectParser();
+        WikipediaRevisionParser revisionParser = new WikipediaRevisionParser();
+        RevisionFormatter formatter = new RevisionFormatter();
+
+        String articleName = inputField.getText();
+        String URL = urlBuilder.BuildUrl(articleName);
+        JSONArray articleJsonArray = wikipediaConnector.connectToWikipedia(URL);
+
+        if(missingParser.parseForMissing(articleJsonArray)) {
+            outputArea.appendText("No Article Found");
+            System.exit(2);
+        }
+
+        outputArea.appendText(redirectParser.parseForRedirect(articleJsonArray));
+
+        ArrayList<Revision> allRevisions = revisionParser.parseForRevisions(articleJsonArray);
+        String formattedRevisions = formatter.format(allRevisions);
+        outputArea.appendText(formattedRevisions);
+
+        System.exit(0);
+    }*/
 }
